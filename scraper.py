63aef48483b02d80a5a4dc9e153abb04d2591568
SonnyFishback <sonny.fishback7@gmail.com>
2020-12-03 22:20:10 -0500

    looping through each stock and retunring data requested

--- scraper.py
+++ scraper.py
@@ -7,26 +7,33 @@ url = "https://ca.finance.yahoo.com/u/yahoo-finance/watchlists/the-berkshire-hat
 r = requests.get(url)
 # Initialize beautiful soup object.
 soup = BeautifulSoup(r.content, "html.parser")
-
 # Select all stocks.
 stocks = soup.select(".cwl-symbols tbody tr")
-# Select one stock by index.
-stock = stocks[0]
+# Array to store each stocks details.
+stock_data_array = []
+# Variable to keep track of which index is being accessed.
+stock_index_count = 0
+# Loop through stocks and store data in stock_data_array.
+for stock in stocks:
+    stock_details_dictionary = dict()
+    
+    stock_details_dictionary["company_name"] = stock.select_one(".data-col1").text.strip()
+    stock_details_dictionary["last_price]"] = stock.select_one(".data-col2").text.strip()
+    stock_details_dictionary["change"] = stock.select_one(".data-col3").text.strip()
+    stock_details_dictionary["percent_change"] = stock.select_one(".data-col4").text.strip()
+    stock_details_dictionary["market_time"] = stock.select_one(".data-col5").text.strip()
+    stock_details_dictionary["volume"] = stock.select_one(".data-col6").text.strip()
+    stock_details_dictionary["average_volume_3_months"] = stock.select_one(".data-col7").text.strip()
+    stock_details_dictionary["market_cap"] = stock.select_one(".data-col8").text.strip()
+    # stock_details_dictionary["stock_details_link_route"] = stock.select_one(".data-col0 a"["href"]
+    # stock_details_dictionary["stock_details_link"] = f"https://ca.finance.yahoo.com{stock_details_dictionary["stock_details_link_route"]}"
+    
+    stock_data_array.append(stock_details_dictionary)
+    print(stock_data_array[stock_index_count])
+    stock_index_count += 1
 
-# Select stock details.
-company_name = stock.select_one(".data-col1").text.strip()
-last_price = stock.select_one(".data-col2").text.strip()
-change = stock.select_one(".data-col3").text.strip()
-percent_change = stock.select_one(".data-col4").text.strip()
-market_time = stock.select_one(".data-col5").text.strip()
-volume = stock.select_one(".data-col6").text.strip()
-average_volume_3_months = stock.select_one(".data-col7").text.strip()
-market_cap = stock.select_one(".data-col8").text.strip()
-stock_details_link_route = stock.select_one(".data-col0 a")["href"]
-stock_details_link = f"https://ca.finance.yahoo.com{stock_details_link_route}"
 
 
 
-print(company_name, last_price, change, percent_change, market_time, volume, average_volume_3_months, market_cap, stock_details_link)
 
 
